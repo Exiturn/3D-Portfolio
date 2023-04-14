b66ad3c495c4a5f2b9a23de40e2ec8cad65ab890
Bryan Magistrado <u1854321@unimail.hud.ac.uk>
2023-04-14 18:57:12 +0100

    BM - Finished the Computers & Loader Components
    Finished off the computers and loader components. Took a break from this project to understand three.js better through the mrsphere project.

--- src/components/canvas/Computers.jsx
+++ src/components/canvas/Computers.jsx
@@ -11,7 +11,20 @@ const Computers = () => {
     <mesh>
       <hemisphereLight intensity={0.15} groundColor="black" />
       <pointLight intensity={0.5} />
-      <primitive object={computer.scene} />
+      <spotLight 
+        position={[-20, 50, 10]}
+        angle={0.12}
+        penumbra={1}
+        intensity={1}
+        castShadow
+        shadow-mapSize={1024}
+      />
+      <primitive 
+        object={computer.scene}
+        scale={0.75}
+        position={[0, -3.25, -1.5]}
+        rotation={[-0.01, -0.2, -0.1]}
+      />
     </mesh>
   )
 }
@@ -24,15 +37,18 @@ const ComputersCanvas = () => {
       camera={{ position: [20, 3, 5], fov: 25 }}
       gl={{ preserveDrawingBuffer: true }}
     >
-      <Suspense>
+      <Suspense fallback={<CanvasLoader />}>
         <OrbitControls
           enableZoom={false}
+          enablePan={false}
           maxPolarAngle={Math.PI / 5}
-          minPolarAngle={Math.PI / 2} />
+          minPolarAngle={Math.PI / 2}
+          autoRotate={true}
+          autoRotateSpeed={3} />
         <Computers />
       </Suspense>
-
       <Preload all />
+      {}
     </Canvas>
   )
 }
